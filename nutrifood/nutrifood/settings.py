587ccddd7f857840d68a4aca2b7eeca57cb81e24
Marc Abelé <[abele.marc@gmail.com](mailto:abele.marc@gmail.com)>
2023-07-05 18:34:25 +0200

    update for quest #5

--- nutrifood/nutrifood/settings.py
+++ nutrifood/nutrifood/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'nutrifood.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'nutrifood_db',
+        'USER': 'marc',
+        'PASSWORD': 'mourc',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
